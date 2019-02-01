35ba8b134c48319959781dd844e52902704339ed
Mary Matthews <matthewsmp@gmail.com>
2019-02-01 14:19:17 -0800

    registration form setup

--- client/src/components/SignUpForm.jsx
+++ client/src/components/SignUpForm.jsx
@@ -6,8 +6,13 @@ class SignUpForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: '',
-      password: ''
+      username: '',
+      password: '',
+      breed: '',
+      birthdate: '',
+      imgUrl: '',
+      name: '',
+      weight: 'in lbs'
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -19,24 +24,30 @@ class SignUpForm extends React.Component {
   }
 
   handleSubmit(event) {
-    axios.post('/cat/register', {
-      username: this.state.email.toLowerCase(),
-      password: this.state.password
-    })
-      // will also need to return an authorization token
-      .then(response => {
-        let user_id = response.data.rows[0].id;
-        this.props.updateUserId(user_id);
-      })
-      .then(() => {
-        this.props.updateView('dashboard');
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+
+    console.log('registration submitted!')
+    // * remember to turn weight from string to number
+    // axios.post('/cat/register', {
+    //   username: this.state.email.toLowerCase(),
+    //   password: this.state.password
+    // })
+    //   // will also need to return an authorization token
+    //   .then(response => {
+    //     let user_id = response.data.rows[0].id;
+    //     this.props.updateUserId(user_id);
+    //   })
+    //   .then(() => {
+    //     this.props.updateView('dashboard');
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
 
     event.preventDefault();
   }
+
+  // form validation to do
+  // only match for numbers in weight
 
   render() {
     return (
@@ -44,8 +55,8 @@ class SignUpForm extends React.Component {
         <h2>Create an account</h2>
         <label className='label'>
           <div>
-            {`Email: `}
-            <input name='email' type="text" value={this.state.email} onChange={this.handleChange} />
+            {`Username: `}
+            <input name='username' type="text" value={this.state.username} onChange={this.handleChange} />
           </div>
         </label>
         <label className='label'>
@@ -54,7 +65,37 @@ class SignUpForm extends React.Component {
             <input name='password' type="text" value={this.state.password} onChange={this.handleChange} />
           </div>
         </label>
-        <input type="submit" className="formComponent" value="Sign up for free" />
+        <label className='label'>
+          <div>
+            {`Breed: `}
+            <input name='breed' type="text" value={this.state.breed} onChange={this.handleChange} />
+          </div>
+        </label>
+        <label className='label'>
+          <div>
+            {`Birthdate: `}
+            <input name='birthdate' type="text" value={this.state.birthdate} onChange={this.handleChange} />
+          </div>
+        </label>
+        <label className='label'>
+          <div>
+            {`Image URL: `}
+            <input name='imageUrl' type="text" value={this.state.imageUrl} onChange={this.handleChange} />
+          </div>
+        </label>
+        <label className='label'>
+          <div>
+            {`Cat's Name: `}
+            <input name='name' type="text" value={this.state.name} onChange={this.handleChange} />
+          </div>
+        </label>
+        <label className='label'>
+          <div>
+            {`Weight: `}
+            <input name='weight' type="text" value={this.state.weight} onChange={this.handleChange} />
+          </div>
+        </label>
+        <input type="submit" className="formComponent" value="Register" />
       </form>
     );
   }
