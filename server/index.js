21b5029c35a0eb183f204f21ba88259c9a8613c5
Mary Matthews <matthewsmp@gmail.com>
2019-02-03 21:16:43 -0800

    enable cors

--- server/index.js
+++ server/index.js
@@ -28,6 +28,13 @@ app.use(function (req, res, next) {
 });
 
 app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.post('/cat/register', (req, res) => {
